974f0932a40eb6cb6ef21c3672ad1230eac36fb2
Wake0036 <stanfordjeanpierre@gmail.com>
2024-07-04 15:57:08 -0400

    End

---
+++ firstProgram.py
@@ -0,0 +1,3 @@
+name = input("Hello! What is your name? ") # Ask for the name
+city = input("Hello "+name+"! Nice to meet you! Where were you born? ") # Ask where the user were born
+print(city+" is nice! I wouldn't mind living there!") # End the program
--- index.py
+++
@@ -1,3 +0,0 @@
-name = input("Hello! What is your name? ")
-city = input("Hello "+name+"! Nice to meet you! Where were you born? ")
-print(city+" is nice! I wouldn't mind living there!")
