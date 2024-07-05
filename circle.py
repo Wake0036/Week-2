76b88d07d9cd1d5b62b5e0b17bab23d94692b2b2
Wake0036 <stanfordjeanpierre@gmail.com>
2024-07-05 00:32:15 -0400

    End

---
+++ circle.py
@@ -0,0 +1,6 @@
+radius = float(input("What is the radius of the circle? ")) # Ask the user the radius and convert it to int
+diameter = radius * 2 # determinate the diameter
+circumference = 2 * 3.14 * radius # determinate the circumference
+area = 3.14 * radius ** 2 # determinate the area 
+print("A circle with a radius of "+str(radius)+" units will have a diameter of "+str(diameter)+" units, a circumference of "
+      +str(circumference)+" units and an area of "+str(area)+" square units.") # end
